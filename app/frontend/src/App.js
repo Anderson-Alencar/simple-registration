7aad31d7ea3027e2dddf1a87816670892770b29c
Anderson-Alencar <anderson23alencar@gmail.com>
2022-06-09 09:42:16 -0300

    :sparkles: table with name, birthDate and age

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import FormAddPerson from './components/FormAddPerson';
+import ListOfPersons from './components/ListOfPersons';
 
 function App() {
   return (
     <div className="App">
       <FormAddPerson />
+      <ListOfPersons />
     </div>
   );
 }
